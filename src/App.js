e399b11550b8958bdec42b3030805849ffbb414f
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-08-27 00:25:53 +0200

    Add NavBar component and integrate react-router-dom for navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Button } from '@mui/material';
 import './App.css';
-
+import NavBar from './components/NavBar.js';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the Practice UI</h1>
-      <Button variant="contained"><AccountCircleIcon /> Profile</Button>
+      <NavBar />
     </div>
   );
 }

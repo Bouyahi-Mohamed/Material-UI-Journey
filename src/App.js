366775fad661653ed5768728cd4088556e5526b8
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-08-26 22:45:12 +0200

    install Material ui and icon ui and style ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Practice UI</h1>
+      <Button variant="contained"><AccountCircleIcon /> Profile</Button>
     </div>
   );
 }
